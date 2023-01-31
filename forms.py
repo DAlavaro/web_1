5d0732277308e6be2af67015aaf33f0e8864a8b7
admin <D.Alavaro@gmail.com>
2023-01-31 22:42:31 +0300

    form CharField

--- forms.py
+++ forms.py
@@ -1,5 +1,7 @@
 from django import forms
 
 class UserForm(forms.Form):
-    name = forms.CharField(label="Имя клиента")
-    age = forms.IntegerField(label="Возраст клиента")
+    name = forms.CharField(label="Имя клиента", max_length=15, help_text="ФИО не более 15 символов", required=False)
+    age = forms.IntegerField(label="Возраст клиента", required=False)
+    basket = forms.BooleanField(label="Положить товар в корзину", required=False)
+    vyb = forms.NullBooleanField(label="Вы поедете в Сочи этим летом?")
