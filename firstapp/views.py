424f60c93b38da8f1652eab6ca661af26177b919
admin <D.Alavaro@gmail.com>
2023-02-07 00:27:22 +0300

    models edit, delete

--- firstapp/views.py
+++ firstapp/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponseNotFound
 from django.shortcuts import render
 from firstapp.models import Person
 
@@ -9,6 +9,7 @@ def index(request):
     return render(request, "firstapp/index.html", {"people": people})
 
 
+# Сохранение данных в БД
 def create(request):
     if request.method == "POST":
         klient = Person()
@@ -16,3 +17,30 @@ def create(request):
         klient.age = request.POST.get("age")
         klient.save()
     return HttpResponseRedirect("/")
+
+
+# Изменение данных в БД
+def edit(request, id_):
+    try:
+        person = Person.objects.get(id=id_)
+        
+        if request.method == "POST":
+            person.name = request.POST.get("name")
+            person.age = request.POST.get("age")
+            person.save()
+            return HttpResponseRedirect("/")
+        else:
+            return render(request, "firstapp/edit.html", {"person": person})
+    except Person.DoesNotExist:
+        return HttpResponseNotFound("<h2>Клиент не найден</h2>")
+
+
+# Удаление из базы данных
+def delete(request, id_):
+    try:
+        person = Person.objects.get(od=id)
+        person.delete()
+        return HttpResponseRedirect("/")
+    except Person.DoesNotExist:
+        return HttpResponseNotFound("<h2>Клиент не найден</h2>")
+
--- hello/urls.py
+++ hello/urls.py
@@ -6,6 +6,8 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('', views.index),
     path('create/', views.create),
+    path('edit/<int:id_', views.edit),
+    path('delete/<int:id_', views.delete),
 
     path('admin/', admin.site.urls),
 ]
