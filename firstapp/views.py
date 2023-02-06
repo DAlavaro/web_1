1448aa92295affc8da53b83b5e13e2325107bcc3
admin <D.Alavaro@gmail.com>
2023-02-06 22:21:44 +0300

    models Create

--- firstapp/views.py
+++ firstapp/views.py
@@ -1,45 +1,18 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from django.http import *
-from django.template.response import TemplateResponse
-from forms import UserForm
+from firstapp.models import Person
 
 
-# Create your views here.
+# Получение данных из БД и загрузка index.html
 def index(request):
+    people = Person.objects.all()
+    return render(request, "firstapp/index.html", {"people": people})
+
+
+def create(request):
     if request.method == "POST":
-        userform = UserForm(request.POST)
-        if userform.is_valid():
-            name = userform.cleaned_data['name']
-            return HttpResponse("<h2>имя введено корректно - {0}</h2>".format(name))
-        else:
-            return HttpResponse('Ошибка ввода данных')
-    else:
-        userform = UserForm()
-        return render(request, "firstapp/index.html", {"form": userform})
-
-    return render(request, "firstapp/index.html", {"form": userform})
-
-
-def about(request):
-    return HttpResponse("About")
-
-
-def contact(request):
-    return HttpResponseRedirect("/about")
-
-
-def details(request):
-    return HttpResponsePermanentRedirect("/")
-
-
-def products(request, productid):
-    category = request.GET.get("cat", "")
-    output = "<h2>Продукт № {0} Категория: {1}</h2>".format(productid, category)
-    return HttpResponse(output)
-
-
-def users(request):
-    id = request.GET.get("id", "1")
-    name = request.GET.get("name", "Максим")
-    output = "<h2>Пользователь</h2><h3>id: {0} Имя:{1}</h3>".format(id, name)
-    return HttpResponse(output)
+        klient = Person()
+        klient.name = request.POST.get("name")
+        klient.age = request.POST.get("age")
+        klient.save()
+    return HttpResponseRedirect("/")
--- hello/urls.py
+++ hello/urls.py
@@ -5,16 +5,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', views.index),
-    path('about/', TemplateView.as_view(template_name="firstapp/about.html")),
-    path('contact/', TemplateView.as_view(
-        template_name="firstapp/contact.html",
-        extra_context={"work": "Разработка программных продуктов"}
-    )),
-    path('details/', views.details),
-
-    path('products/<int:productid>/', views.products),
-
-    path('users/', views.users),
+    path('create/', views.create),
 
     path('admin/', admin.site.urls),
 ]
