6437fdb19442cbf71123efca94896a77fa26d230
DAlavaro <d.alavaro@gmail.com>
2023-02-08 15:05:20 +0300

    many to many and one to one

--- firstapp/models.py
+++ firstapp/models.py
@@ -16,3 +16,24 @@ class Product(models.Model):
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
     name = models.CharField(max_length=30)
     price = models.IntegerField()
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=30)
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=30)
+    courses = models.ManyToManyField(Course)
+
+
+class User(models.Model):
+    name = models.CharField(max_length=20)
+
+
+class Account(models.Model):
+    login = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+    user = models.OneToOneField(User,
+                                on_delete=models.CASCADE,
+                                primary_key=True)
